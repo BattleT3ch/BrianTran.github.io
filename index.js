059dd740fa76b669788ba0802a3a75f3ddff4a23
BattleT3ch <98723566+BattleT3ch@users.noreply.github.com>
2023-02-12 11:27:07 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+<html lang="en-US">
+    <meta charset="utf-8"/>
+<main>
+    <body>
+    <h1>Yugioh </h1> 
+    <p> <a href="https://www.konami.com/yugioh/masterduel/us/en/" title = "Master Duel Homepage"> Yugioh Master Duel</a> 
+    is a card game about two player summoning monsters and using them to attack the other player until their <strong> LP</strong> 
+    or <strong>Life Point </strong> drops to 0. However, this is a basic explanation of the game. 
+    For example, <strong> monsters </strong> come with effects, making even the weakest monster stat wise, a threat. 
+    And there are cards like <strong> Spell </strong> and <strong>Trap</strong> cards which can either support or hinder your opponent. </p>
+    <img src="https://ravegrl.files.wordpress.com/2022/01/yugioh_master_duel_logo_ten_thousand_dragon.jpg?w=640"></img>  
+    </body>
+</main>
+</html>
